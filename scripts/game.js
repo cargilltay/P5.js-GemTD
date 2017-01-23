fbb5ef67ef264ce722cd9b8a2122a98a5b8d6ca9
Taylor Cargill <tcargill@bizstream.com>
2017-01-23 18:22:35 -0500

    grid setup for 37x37

--- scripts/game.js
+++ scripts/game.js
@@ -1,19 +1,39 @@
 var cols, rows;
-var w = 40;
-
+var w = 20;
+var grid = [];
 
 function setup() {
-	createCanvas(400,400);
-	cols = floor(width/w);
-	rows = floor(height/w);
+	createCanvas(740, 740);
+	cols = floor(width / w);
+	rows = floor(height / w);
+
+	for (var i = 0; i < rows; i++) {
+		for (var j = 0; j < cols; j++) {
+			var cell = new Cell(i, j);
+			grid.push(cell)
+		}
+	}
 }
 
 
 function draw() {
 	background(51);
+	for (var i = 0; i < grid.length; i++) {
+		grid[i].show();
+	}
 }
 
-function Cell(colNum, rowNum){
+function Cell(colNum, rowNum) {
 	this.colNum = colNum;
 	this.rowNum = rowNum;
+
+	this.show = function() {
+		var x = this.colNum * w;
+		var y = this.rowNum * w;
+		stroke(255);
+
+		//walls
+		noFill();
+		rect(x, y, w, w);
+	}
 }
