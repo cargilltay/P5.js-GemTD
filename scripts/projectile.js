fb0a4e6949fc5b042a885bf3a577ee7be613dbe9
Taylor Cargill <cargillt@mail.gvsu.edu>
2017-03-17 15:27:52 -0400

    modify offset check code

--- scripts/projectile.js
+++ scripts/projectile.js
@@ -17,10 +17,12 @@ function Projectile(x, y, target) {
 	this.checkHit = function(){
 		var tarX = this.target.pos.x;
 		var tarY = this.target.pos.y;
-		var pixelOffset = 2;
+		var checkWithinPixelOffset = 10;
+		var isWithinOffsetOfX  =  (this.x >= tarX - checkWithinPixelOffset && this.x <= tarX + checkWithinPixelOffset) ? true : false;
+		var isWithinOffsetOfY  =  (this.y >= tarY - checkWithinPixelOffset && this.y <= tarY + checkWithinPixelOffset) ? true : false;
 
-		//check if within 2 pixels
-		if ((this.x >= tarX - pixelOffset && this.x <= tarX + pixelOffset) && (this.y >= tarY - pixelOffset && this.y <= tarY + pixelOffset)) {
+		//check if within offset
+		if (isWithinOffsetOfX && isWithinOffsetOfY) {
 			this.target.hitPoints -= this.damage;
 			this.hitTarget = true;
 			if (this.target.hitPoints == 0) {
@@ -38,7 +40,7 @@ function Projectile(x, y, target) {
 
 		//console.log(this.target.pos.x)
 		//console.log(this.x)
-		if(this.checkHit){
+		if(this.checkHit()){
 			return;
 		}
 
